c17e7e00dc9fb11589edc1c3bb461ec252e8400c
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-08 22:05:13 +0100

    Practice session after "Our First Application" section: - Goal: try various functions of Console class - just for getting used to code - Result: small console app where: -- User types in theme name of choice -- If theme name is valid it applies, else user is asked to type theme name again -- Users choise is logged into text file

---
+++ ConsolePractice/Program.cs
@@ -0,0 +1,58 @@
+﻿// Console View alterations
+
+bool isThemeChosen = false;
+string? chosenThemeName = null;
+
+while (!isThemeChosen) 
+{
+    Console.WriteLine(@"Select a theme: type ""light"", ""dark"" or ""matrix""");
+
+    chosenThemeName = Console.ReadLine();
+    isThemeChosen = TrySetTheme(chosenThemeName);
+}
+
+//Changing out stream for logging, logging and closing output
+
+StreamWriter logWriter = new StreamWriter(@".\Log.txt");
+logWriter.AutoFlush = true;
+Console.SetOut(logWriter);
+Console.Out.WriteLine($"{DateTime.UtcNow}: {chosenThemeName} theme chosen");
+Console.Out.Close();
+
+//Changing out stream back to console standard
+
+StreamWriter standartOutput = new StreamWriter(Console.OpenStandardOutput());
+standartOutput.AutoFlush = true;
+Console.SetOut(standartOutput);
+Console.WriteLine($"{chosenThemeName} theme is successfully chosen");
+
+//Reading logs file contents
+StreamReader logReader = new StreamReader(@".\Log.txt");
+
+Console.ReadLine();
+
+bool TrySetTheme(string? themeName) 
+{
+    switch (themeName)
+    {
+        case "light":
+            SetTheme(ConsoleColor.White, ConsoleColor.Black);
+            return true;
+        case "dark":
+            SetTheme(ConsoleColor.Black, ConsoleColor.White);
+            return true;
+        case "matrix":
+            SetTheme(ConsoleColor.Black, ConsoleColor.Green);
+            return true;
+        default:
+            Console.WriteLine("Invalid theme name, try again\n");
+            return false;
+    }
+}
+
+void SetTheme(ConsoleColor backgroundColor, ConsoleColor foregroundColor) 
+{
+    Console.BackgroundColor = backgroundColor;
+    Console.ForegroundColor = foregroundColor;
+    Console.Clear();
+}
