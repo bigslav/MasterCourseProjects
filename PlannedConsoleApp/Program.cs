aa0362101aaa8cc9a670c4a14281daa11a4f8e9b
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-09 21:09:09 +0100

    Practice session after "Our Second Application" -> "Planning Out the Application" lesson Goal: - plan out simple application - develop it following plan Result: - planned out key-value repository - developed partly and not the most part - initial idea with commands that can be run at any time app is running seems overcomplicated - todo: simplify the idea, focus only on writing and reading from file, keeping data in the file without overrides

---
+++ PlannedConsoleApp/Program.cs
@@ -0,0 +1,65 @@
+﻿// "Library" - user can write down text key-value pairs and can output values by writing keys
+
+// Breakdown into steps
+
+// Reserve special characters for "command" and "exit"
+// Command => restart command scenario
+// Exit => exit app
+
+// Command scenario
+// Ask for command
+// command == set
+// // ask for key => repeat if invalid input
+// // ask for value => repeat if invalid input
+// command == get
+// // ask for key => repeat if invalid input
+
+
+Console.WriteLine("Write command to restart request\n" +
+                  "Write exit to exit program\n");
+
+bool isExitCommand = false;
+
+while (!isExitCommand) 
+{
+    Console.WriteLine("Write set to make key-value entry\n" +
+                      "Write get to get entry value by key\n");
+
+
+    string? userInput = RequestInput();
+
+    switch (userInput)
+    {
+        case "exit":
+            isExitCommand = true;
+            break;
+        case "command":
+            break;
+        case "get":
+            // start get
+            break;
+        case "set":
+            // start set
+            break;
+        default:
+            break;
+    }
+}
+
+string? RequestInput() 
+{
+    bool isValidInput = false;
+    string? userInput = "";
+
+    while (!isValidInput) 
+    { 
+        Console.Write(">> ");
+        
+        userInput = Console.ReadLine();
+
+        if (!String.IsNullOrEmpty(userInput))
+            isValidInput = true;
+    }
+
+    return userInput;
+}
