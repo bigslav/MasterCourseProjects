cb62dab1d5fe4c08fd7711681dccb44459acb449
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-15 22:23:00 +0100

    Practice after Decimal lesson

---
+++ DecimalPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿decimal test = new decimal();
+
+// default value is 0M
+Console.WriteLine(test);
+
+// test = 10.1 gives compiler error, implicit conversion of double to decimal can lead to lose of data
+// works both ways and with all floating point types (only exception: float -> double)
+
+float a = 1.0f;
+double b = a;
+
+Console.WriteLine($@"minValue: {decimal.MinValue}
+maxValue: {decimal.MaxValue}");
+
+// decimal has more precision and a smaller range than other floating point types
+// used in financial calculations for example
