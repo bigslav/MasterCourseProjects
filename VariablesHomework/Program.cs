2adf6e673098e36052fcaedb87f3d9131044f0f3
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-17 20:09:26 +0100

    Variables Homework

---
+++ VariablesHomework/Program.cs
@@ -0,0 +1,28 @@
+﻿Person person = new Person("Billy", 24, true, "911");
+
+Console.WriteLine($@"Name: {person.name}
+Age: {person.age}
+Is alive: {person.isAlive}
+Phone number: {person.phoneNumber}");
+
+public class Person
+{
+    public string name => _name;
+    // age can't be less than zero and can't be very big, can use ushort for micro-optimization
+    public ushort age => _age;
+    public bool isAlive => _isAlive;
+    public string phoneNumber => _phoneNumber;
+
+    string _name;
+    ushort _age;
+    bool _isAlive;
+    string _phoneNumber;
+
+    public Person(string name, ushort age, bool isAlive, string phoneNumber)
+    {
+        _name = name;
+        _age = age;
+        _isAlive = isAlive;
+        _phoneNumber = phoneNumber;
+    }
+}
