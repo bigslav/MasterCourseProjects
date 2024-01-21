ac6f8e50de37eaf4ef8302acb1f108380818359d
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-21 16:42:42 +0100

    Practice after Type Conversions practice

---
+++ TypeConversionsPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+bool isValidInput = false;
+bool isAdult = false;
+
+while (!isValidInput) 
+{
+    // will throw an exception if input is not bool (true or false, case insensitive)
+    // bool isAdult = bool.Parse(Console.ReadLine());
+
+    if (!isValidInput)
+        Console.Write("Are you male (true or false): ");
+
+    isValidInput = bool.TryParse(Console.ReadLine(), out isAdult);
+}
+
+Console.WriteLine(isAdult ? "You are male" : "You are not male");
