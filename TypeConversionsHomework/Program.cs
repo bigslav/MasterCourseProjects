924d9fb6d5a36dcdb41678a4eaefdf6d0703a854
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-22 19:19:09 +0100

    Type Conversions homework

---
+++ TypeConversionsHomework/Program.cs
@@ -0,0 +1,24 @@
+﻿const int ageGap = 25;
+
+bool isInputValid = false;
+int age = 0;
+
+while (!isInputValid)
+{
+    Console.WriteLine("How old are you?");
+    isInputValid = int.TryParse(Console.ReadLine(), out age);
+
+    if (!isInputValid)
+        Console.WriteLine("Invalid input, try again");
+    
+    Console.WriteLine();
+}
+
+int ageMinusGap = age - ageGap;
+Console.WriteLine(ageMinusGap >= 0 ? $"You were {ageMinusGap} years old {ageGap} years ago" 
+                  : $"You were not born yet {ageGap} years ago");
+
+int agePlusGap = age + ageGap;
+Console.WriteLine($"In {ageGap} years you would be {agePlusGap} years");
+
+Console.ReadLine();
