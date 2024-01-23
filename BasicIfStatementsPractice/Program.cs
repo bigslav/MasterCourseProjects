752f4265293ea483a8344c744177dce615569e6f
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-23 20:05:58 +0100

    Practice after basic if statements lesson

---
+++ BasicIfStatementsPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿
+bool isInputValid = false;
+int number = 0;
+
+while (!isInputValid)
+{
+    Console.Write("Pick a number: ");
+    isInputValid = int.TryParse(Console.ReadLine(), out number);
+
+    if(!isInputValid)
+        Console.WriteLine("Not a number\n");
+}
+
+if (number > 0)
+    Console.WriteLine("Number is positive");
+else if (number < 0)
+    Console.WriteLine("Number is negative");
+else
+    Console.WriteLine("Number is zero");
+
+switch (number)
+{
+    case 0:
+        Console.WriteLine("ZERO");
+        break;
+    case 100:
+        Console.WriteLine("HUNDRED");
+        break;
+    default:
+        Console.WriteLine("Not a zero or a hundred");
+        break;
+}
