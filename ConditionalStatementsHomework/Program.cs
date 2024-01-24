c61fc47038b88f997df779b16cb17b21e0759493
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-24 19:46:43 +0100

    Conditional statements homework

---
+++ ConditionalStatementsHomework/Program.cs
@@ -0,0 +1,26 @@
+﻿bool isValidInput = false;
+string name = String.Empty;
+
+while (!isValidInput) 
+{
+    Console.WriteLine("What's your name: ");
+    name = Console.ReadLine();
+
+    if(!string.IsNullOrEmpty(name))
+        isValidInput = true;
+}
+
+if (name.ToLower() == "tim")
+    Console.WriteLine("Hello professor!");
+else
+    Console.WriteLine("Hello student!");
+
+switch (name.ToLower())
+{
+    case "tim":
+        Console.WriteLine("Hello professor!");
+        break;
+    default:
+        Console.WriteLine("Hello student!");
+        break;
+}
