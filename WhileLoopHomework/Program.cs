eb28ac91bd2cb1703436512b9a05ed7a340757bd
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-25 19:29:20 +0100

    Homework after While loop lesson

---
+++ WhileLoopHomework/Program.cs
@@ -0,0 +1,27 @@
+﻿string userInput = string.Empty;
+bool isExiting = false;
+
+do
+{
+    Console.WriteLine("Name: ");
+    userInput = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(userInput))
+        continue;
+
+    switch (userInput.ToLower())
+    {
+        case "tim":
+            Console.WriteLine("Hi Tim!");
+            break;
+        case "exit":
+            Console.WriteLine("Exiting...");
+            isExiting = true;
+            break;
+        default:
+            Console.WriteLine($"Hi student {userInput}!");
+            break;
+    }
+} while (!isExiting);
+
+Console.ReadLine();
