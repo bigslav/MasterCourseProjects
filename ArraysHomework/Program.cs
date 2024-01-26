313279072d57c0ea92b73f7f6e87da7de79c2645
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-26 18:30:36 +0100

    Arrays homework

---
+++ ArraysHomework/Program.cs
@@ -0,0 +1,26 @@
+﻿string namesData = "Bob+Bib+Bab";
+
+string[] names = namesData.Split('+');
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Name {i} is {names[i]}");
+}
+
+int chosenIndex;
+bool isValidInput;
+
+do
+{
+    Console.Write("Type name index: ");
+    isValidInput = int.TryParse(Console.ReadLine(), out chosenIndex);
+
+    if (chosenIndex < 0
+        || chosenIndex > names.Length - 1)
+    {
+        Console.WriteLine("Wrong index");
+        isValidInput = false;
+    }
+} while (!isValidInput);
+
+Console.WriteLine($"You chose {names[chosenIndex]}");
