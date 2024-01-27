91ef62013f3649b20dc906d3d14f06d7b60bf99d
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-27 14:53:21 +0100

    Homework after For Loops lesson

---
+++ ForLoopsHomework/Program.cs
@@ -0,0 +1,28 @@
+﻿string userInput = string.Empty;
+bool isInputValid = false;
+List<string> names = new();
+
+do
+{
+    Console.WriteLine("Write a comma-separated list of names:");
+    userInput = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(userInput))
+    {
+        isInputValid = true;
+
+        names = userInput.Split(',').ToList();
+
+        for (int i = 0; i < names.Count; i++)
+        {
+            names[i] = names[i].Trim();
+        }
+    }
+} while (!isInputValid);
+
+for (int i = 0; i < names.Count; i++) 
+{
+    Console.WriteLine($"Hello {names[i]}");
+}
+
+Console.ReadLine();
