7e7e21227c2da28f083de55dbe6de8d4130d6ce3
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-29 19:01:21 +0100

    Practice after Tuples lesson

---
+++ TuplesPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿// data variable of tuple type, get elements with data.ItemN
+(int, int) data = GetSumAndMultiplication(2, 3);
+Console.WriteLine($"sum: {data.Item1}, mult: {data.Item2}");
+
+// items can be renamed
+(int sum, int mult) dataNamedElements = GetSumAndMultiplication(2, 3);
+Console.WriteLine($"sum: {dataNamedElements.sum}, mult: {dataNamedElements.mult}");
+
+// can store returned values in separate variables initialized inside tuple
+(int sum, int mult) = GetSumAndMultiplication(2, 3);
+Console.WriteLine($"sum: {sum}, mult: {mult}");
+
+//discard unused returned value
+(int sumOnly, _) = GetSumAndMultiplication(2, 3);
+Console.WriteLine($"sum only: {sumOnly}");
+
+(int, int) GetSumAndMultiplication(int x, int y)
+{
+    return (x + y, x * y);
+}
