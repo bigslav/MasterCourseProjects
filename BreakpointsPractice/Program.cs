0220f20fe7cf15cc47310f2df4a1bf6d819796ac
bigslav <sviatoslav.steshenko@gmail.com>
2024-01-31 19:05:57 +0100

    Exceptions practice

---
+++ BreakpointsPractice/Program.cs
@@ -0,0 +1,8 @@
+﻿int total = 0;
+
+// values and code can be modified while debugging
+// new breakpoints can be set while debugging
+for (int i = 0; i < 10; i++)
+{
+    total += i * 5;
+}
---
+++ ExceptionsPractice/Program.cs
@@ -0,0 +1,59 @@
+﻿try
+{
+    BadCall();
+    // is ignored, because of an exception in a first call
+    BadCallWithInfo();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
+
+Console.ReadLine();
+
+try
+{
+    BadCallWithInfo();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
+
+void BadCall()
+{
+    int[] array = { 1, 2, 3, 4 };
+
+    for (int i = 0; i < 5; i++)
+    {
+        try
+        {
+            Console.WriteLine(array[i]);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("BadCall");
+            throw;
+        }
+    }
+}
+
+void BadCallWithInfo()
+{
+    int[] array = { 1, 2, 3, 4 };
+
+    for (int i = 0; i < 5; i++)
+    {
+        try
+        {
+            Console.WriteLine(array[i]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("BadCallWithInfo");
+            throw new Exception("Outside of bounds of array", e);
+        }
+    }
+}
