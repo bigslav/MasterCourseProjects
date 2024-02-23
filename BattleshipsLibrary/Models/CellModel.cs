d1e2e45c826ef797ab75fb5d10670c54a652a112
bigslav <sviatoslav.steshenko@gmail.com>
2024-02-23 17:12:33 +0100

    add filling in player map process

--- BattleshipsLibrary/Models/CellModel.cs
+++ BattleshipsLibrary/Models/CellModel.cs
@@ -1,5 +1,3 @@
-using System.Numerics;
-
 namespace BattleshipsLibrary.Models;
 
 public enum CellState
@@ -12,17 +10,11 @@ public enum CellState
 public class CellModel
 {
     public CellState CellState => _cellState;
-    public int VerticalPosition => _verticalPosition;
-    public int HorizontalPosition => _horizontalPosition;
 
-    readonly int _verticalPosition;
-    readonly int _horizontalPosition;
     CellState _cellState;
 
-    public CellModel(int verticalPosition, int horizontalPosition, bool hasShip)
+    public CellModel(bool hasShip)
     {
-        _verticalPosition = verticalPosition;
-        _horizontalPosition = horizontalPosition;
         _cellState = hasShip ? CellState.ShipHealthy : CellState.Empty;
     }
 
--- BattleshipsUI/Program.cs
+++ BattleshipsUI/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BattleshipsLibrary.Models;
+using ConsoleRequestDataLibrary;
+
+const int fieldSize = 5;
+const int maxShipCount = 5;
+
+CellModel[,] playerMap = new CellModel[fieldSize,fieldSize];
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < fieldSize; j++)
+    {
+        playerMap[i, j] = new CellModel(false);
+    }
+}
+
+Console.WriteLine("Fill ships:");
+for (int i = 1; i <= maxShipCount; i++)
+{
+    bool canPlaceShip = false;
+
+    do
+    {
+        int horizontalPosition = RequestData.RequestInt($"Ship {i}: type in ship's horizontal position from 1 to {fieldSize}", IntType.PositiveOnly);
+        int verticalPosition = RequestData.RequestInt($"Ship {i}: type in ship's vertical position from 1 to {fieldSize}", IntType.PositiveOnly);
+
+        if (playerMap[horizontalPosition, verticalPosition].CellState == CellState.Empty)
+        {
+            playerMap[horizontalPosition, verticalPosition].SetState(CellState.ShipHealthy);
+            canPlaceShip = true;
+        }
+        else
+        {
+            Console.WriteLine("Ship spot is already taken!");
+        }
+    } while (!canPlaceShip);
+}
