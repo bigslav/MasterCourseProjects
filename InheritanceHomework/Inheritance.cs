cb8f9e5f66abb7f20d9b5fa2a90d12acbb739a14
bigslav <sviatoslav.steshenko@gmail.com>
2024-04-27 19:28:32 +0200

    Inheritance homework

---
+++ InheritanceHomework/Inheritance.cs
@@ -0,0 +1,43 @@
+﻿namespace InheritanceHomework;
+
+public class Vehicle
+{
+    public int passengersCount { get; set; }
+
+    public void Move()
+    {
+    }
+
+    public void Stop()
+    {
+    }
+}
+
+public class Car : Vehicle
+{
+    public int wheelsCount { get; set; }
+    public bool hasRoof { get; set; }
+
+    public void FastenSeatBelt()
+    {
+    }
+}
+
+public class Boat : Vehicle
+{
+    public bool isMechanical;
+    public bool isUnderwater;
+
+    public void DropAnchor()
+    {
+    }
+}
+
+public class Motorcycle : Vehicle
+{
+    public bool isMountain;
+
+    public void RideOnBackWheel()
+    {
+    }
+}
