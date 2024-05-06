977831e6b7cf8914e6dd4d080611bbdd98ddc873
bigslav <sviatoslav.steshenko@gmail.com>
2024-05-06 13:16:24 +0200

    fix

---
+++ InterfacesHomework/Program.cs
@@ -0,0 +1,13 @@
+﻿using InterfacesHomework;
+
+var runners = new List<IRun>
+{
+    new Animal(10),
+    new Animal(3),
+    new Person(5)
+};
+
+foreach (IRun runner in runners)
+{
+    runner.Run();
+}
