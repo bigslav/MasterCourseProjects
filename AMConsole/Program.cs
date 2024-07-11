24c5ebd330dc672466936ecccd5ec9a64734ef88
bigslav <sviatoslav.steshenko@gmail.com>
2024-07-11 14:53:30 +0200

    AM homework (lost commit)

---
+++ AMConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AMLibrary;
+
+Console.WriteLine("Hello, World!");
+
+PublicClass publicClass = new PublicClass();
+publicClass.PublicField = 0; // has only access to public prop
+
+public class PublicClassChildInOtherAssembly : PublicClass
+{
+    public PublicClassChildInOtherAssembly()
+    {
+        PublicField = 0; // has access as it's public
+        ProtectedField = 0; // has access as it is accessed from a derived class
+        ProtectedInternalField = 0; // Internal, but has access as it is accessed from a derived class in another assembly (protected internal)
+    }
+}
---
+++ AMLibrary/Classes.cs
@@ -0,0 +1,26 @@
+﻿namespace AMLibrary;
+
+public class PublicClass
+{
+    public int PublicField { get; set; } // accessed anywhere in this and other assemblies
+    int privateValue; // access only inside this class
+    protected int ProtectedField { get; set; } // accessed in this or derived class in this and other assemblies
+    protected internal int ProtectedInternalField { get; set; } // accessed in this assembly and for parent and derived classes in other assemblies
+    private protected int PrivateProtectedField { get; set; } // accessed only in this assembly and for parent and derived classes
+}
+
+public class PublicClassChild : PublicClass
+{
+    PublicClass _publicClass;
+
+    public PublicClassChild()
+    {
+        PublicField = 0; // has access
+        ProtectedField = 0; // has access
+        ProtectedInternalField = 0; // has access
+        PrivateProtectedField = 0;
+
+        _publicClass = new PublicClass();
+        _publicClass.ProtectedInternalField = 0; // has access due to Internal part, also can be accessed in a derived class in another assembly
+    }
+}
